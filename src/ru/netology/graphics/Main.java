2cc158738e5ac8fd53e38d24f66c702f4d6b13bc
Aleksandr Koloshtivin <aykolosh@mts.ru>
2021-09-20 21:33:30 +0300

    Init repo

---
+++ src/ru/netology/graphics/Main.java
@@ -0,0 +1,26 @@
+package ru.netology.graphics;
+
+import ru.netology.graphics.image.Converter;
+import ru.netology.graphics.image.TextGraphicsConverter;
+import ru.netology.graphics.server.GServer;
+
+import java.io.File;
+import java.io.PrintWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        TextGraphicsConverter converter = new Converter(); // Создайте тут объект вашего класса конвертера
+
+//        GServer server = new GServer(converter); // Создаём объект сервера
+//        server.start(); // Запускаем
+
+        // Или то же, но с сохранением в файл:
+
+        PrintWriter fileWriter = new PrintWriter(new File("converted-image.txt"));
+        converter.setMaxWidth(200);
+        converter.setMaxHeight(300);
+        fileWriter.write(converter.convert("https://i.ibb.co/pv8Mzhc/GNs-L5p-K-jb-A.jpg"));
+        fileWriter.close();
+
+    }
+}
---
+++ src/ru/netology/graphics/image/Schema.java
@@ -0,0 +1,17 @@
+package ru.netology.graphics.image;
+
+public class Schema implements TextColorSchema {
+    private char[] initSchema = new char[]{
+            '▇', '●', '◉', '◍', '◎', '○', '☉', '◌', '-'
+    };
+    /**
+     * получение среднего промежутка для каждого символа из массива с добавлением единицы,
+     * чтобы забрать оставшиеся элементы
+     */
+    private int charCoeffToGet = (255 / initSchema.length) + 1;
+
+    @Override
+    public char convert(int color) {
+        return initSchema[initSchema.length - 1 - (color / charCoeffToGet)];
+    }
+}
